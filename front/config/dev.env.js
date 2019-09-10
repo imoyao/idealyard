cc9bac7847e6bb1e045dc206375ebbacbb7d08c0
imoyao <emailme8@163.com>
2019-09-10 21:49:38 +0800

    turn on gzip

---
+++ front/config/dev.env.js
@@ -0,0 +1,10 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  // BASE_API: '"http://localhost:8888/"'
+  // BASE_API: '"http://192.168.116.21:5000/api"'
+  BASE_API: '"http://192.168.31.133:5000/api"'
+})
