2cab7ac58e0591e097f36ac740fec92f510f2d3a
imoyao <emailme8@163.com>
2019-09-12 00:14:21 +0800

    turn off history

--- front/config/index.js
+++ front/config/index.js
@@ -57,7 +57,6 @@ module.exports = {
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
-
     productionGzip: true,   // 开启gzip压缩
     productionGzipExtensions: ['js', 'css'],
 
