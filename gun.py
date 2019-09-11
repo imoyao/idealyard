7355b8779d94a1ab35174dcb8ed5df886933c369
imoyao <emailme8@163.com>
2019-09-11 22:14:23 +0800

    update gun conf

--- gun.py
+++ gun.py
@@ -35,9 +35,9 @@ raw_env = [
     # 开启DEBUG模式 **注意**：生产模式下必须关闭
     'FLASK_DEBUG=True',
     # 配置工作模式（此处默认开发模式）
-    'FLASK_ENV=development',
+    'FLASK_ENV=production',
     # 使用的配置环境（默认使用生产配置）
-    'FLASK_CONFIG=default'
+    'FLASK_CONFIG=production'
 ]
 # 项目的根目录,比如你的app.py文件在/home/ubuntu/app目录下，就填写'/home/ubuntu/app'
 # chdir = ''
